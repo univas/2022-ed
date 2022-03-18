0a7c2c8abf86cec8b993de14aea8d6413c1b5639
Roberto Rocha <rrocha.roberto@gmail.com>
2022-03-17 22:06:23 -0300

    Implementação da pilha

---
+++ Projeto02/src/br/edu/univas/si3/ed/pilha/Pilha.java
@@ -0,0 +1,31 @@
+package br.edu.univas.si3.ed.pilha;
+
+import br.edu.univas.si3.ed.pilha.dados.Livro;
+
+//Tarefa para casa: desenhar o passo-a-passo de cada um dos métodos desta classe.
+public class Pilha {
+
+	private No topo;
+	
+	public void inserir(Livro novoLivro) { //push
+		No novoNo = new No();
+		novoNo.item = novoLivro;
+		novoNo.abaixo = topo;
+		topo = novoNo;
+	}
+	
+	public Livro remover() {//pop
+		No removido;
+		removido = topo;
+		
+		if(!estaVazia()) {
+			topo = topo.abaixo;
+			return removido.item;
+		}
+		return null;
+	}
+	
+	public boolean estaVazia() {
+		return topo == null;
+	}
+}
---
+++ Projeto02/src/br/edu/univas/si3/ed/pilha/dados/Livro.java
@@ -0,0 +1,6 @@
+package br.edu.univas.si3.ed.pilha.dados;
+
+public class Livro {
+
+	public String titulo;
+}
