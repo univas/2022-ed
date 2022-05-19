62ed0f3fe84320d503c8a67105b2882505b02551
Roberto Rocha <rrocha.roberto@gmail.com>
2022-05-18 22:08:43 -0300

    Implementando travessia em-ordem e pós-ordem

--- Projeto04/src/br/edu/univas/si3/ed/arvore/model/ArvoreBinariaV1.java
+++ Projeto04/src/br/edu/univas/si3/ed/arvore/model/ArvoreBinariaV1.java
@@ -22,17 +22,36 @@ public class ArvoreBinariaV1 {
 	
 	public void imprimir() {
 		System.out.println("Iniciando impressão da árvore.");
-		imprimirPreOrdem(raiz, "$");
+		//imprimirPreOrdem(raiz, "$");
+		imprimirEmOrdem(raiz);
 	}
 	
 	//método recursivo
-	public void imprimirPreOrdem(No raizDaSubArvore, String cenario) {
-		System.out.println("Cenário: " + cenario);
+	private void imprimirPreOrdem(No raizDaSubArvore, String cenario) {
+		//System.out.println("Cenário: " + cenario);
 		if(raizDaSubArvore == null) {
 			return;
 		}
 		System.out.println(cenario + " : " + raizDaSubArvore.entidade.nome);
 		imprimirPreOrdem(raizDaSubArvore.esquerda, cenario + "<"); 
 		imprimirPreOrdem(raizDaSubArvore.direita, cenario + ">");
+	}
+	
+	private void imprimirEmOrdem(No raizDaSubArvore) {
+		if(raizDaSubArvore == null) {
+			return;
+		}
+		imprimirEmOrdem(raizDaSubArvore.esquerda);
+		System.out.println(raizDaSubArvore.entidade.nome);
+		imprimirEmOrdem(raizDaSubArvore.direita);
+	}
+	
+	private void imprimirPosOrdem(No raizDaSubArvore) {
+		if(raizDaSubArvore == null) {
+			return;
+		}
+		imprimirPosOrdem(raizDaSubArvore.esquerda);
+		imprimirPosOrdem(raizDaSubArvore.direita);
+		System.out.println(raizDaSubArvore.entidade.nome);
 	}
 }
